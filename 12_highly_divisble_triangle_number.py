2da4d6de4e95e18fcf83088a570626eb62c9d71e
Julius Cueto <juliuscueto@gmail.com>
2018-06-06 11:37:54 +0900

    [mod] 12

--- 12_highly_divisble_triangle_number.py
+++ 12_highly_divisble_triangle_number.py
@@ -9,8 +9,11 @@ def primesfrom2to(n):
             sieve[k*(k-2*(i&1)+4)//3::2*k] =False
     return np.r_[2,3,((3*np.nonzero(sieve)[0][1:]+1)|1)]
 
-def factor(n):
-    l_p = primesfrom2to(n)
+def factor(n, primes=False):
+    if primes:
+        l_p = primes
+    else:
+        l_p = primesfrom2to(n)
     l = np.zeros_like(l_p)
     for i in range(len(l_p)):
         while n % l_p[i] == 0:
@@ -23,12 +26,13 @@ def factor(n):
 def nth_triangle_number(n):
     return n*(n+1)//2
 
-def num_of_divisor(n):
-    l_factor = factor(n)
+def num_of_divisor(n, primes=False):
+    l_factor = factor(n, primes)
     return np.prod(l_factor[np.nonzero(l_factor)] + 1)
 
 if __name__ == "__main__":
     i = 1
+    primes = primesfrom2to(nth_triangle_number(1000000))
     while True:
         if num_of_divisor(nth_triangle_number(i)) > 500:
             ret = nth_triangle_number(i)
